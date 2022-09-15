ff52a385b42d7f840964e76ccf6afdabde8f579a
habanerocity <lindyo87@gmail.com>
2022-09-14 18:37:21 -0700

    added product modal and fixed bs styles

--- assets/application.js
+++ assets/application.js
@@ -33,7 +33,6 @@ if (document.getElementById('AddressCountryNew') != null) {
 
 if (document.getElementById('forgotPassword') != null) {
     document.getElementById("forgotPassword").addEventListener("click", function (e) {
-        console.log('i clicked');
         const element = document.querySelector("#forgot_password_form");
         if (element.classList.contains('d-none')) {
             element.classList.remove("d-none");
@@ -41,3 +40,58 @@ if (document.getElementById('forgotPassword') != null) {
         }
     })
 }
+
+const productInfoAnchors = document.querySelectorAll("#productInfoAnchor");
+
+const productModal = new bootstrap.Modal(document.getElementById('productInfoModal'), {});
+
+if (productInfoAnchors.length > 0) {
+    productInfoAnchors.forEach(item => {
+        item.addEventListener('click', e => {
+            const url = '/products/' + item.getAttribute('product-handle') + '.js';
+
+            fetch(url).then((res) => res.json()).then((data) => {
+                console.log(data);
+
+                document.getElementById("productInfoImg").src = data.images[0];
+                document.getElementById("productInfoTitle").innerHTML = data.title;
+                document.getElementById("productInfoPrice").innerHTML = item.getAttribute('product-price');
+                document.getElementById("productInfoDescription").innerHTML = data.description;
+
+                const variants = data.variants;
+                const variantSelect = document.getElementById("modalItemID");
+
+                variants.forEach(function (variant, index) {
+                    console.log(variant);
+
+                    variantSelect.options[variantSelect.options.length] = new Option(variant.option1, variant.id);
+                })
+                productModal.show();
+            })
+        })
+    })
+}
+
+const modalAddToCartForm = document.querySelector('#addToCartForm');
+modalAddToCartForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = {
+        "items": [
+            {
+                'id': document.getElementById("modalItemID").value,
+                'quantity': document.getElementById("modalItemQuantity").value
+            }
+        ]
+    };
+
+    fetch('/cart/add.js', {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(formData)
+    }).then((res) => res.json()).catch((err) => {
+        console.error('Error: ' + err);
+    })
+})
